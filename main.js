b7c7543b777937079cc1bbfd475d68722f5d80c0
Juli Watson <juliwatson@gmail.com>
2013-07-02 22:17:08 -0400

    Update main.js
    added objects and error messages and class to completed items

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
+    var tasks = [{
+        'name' : 'Do homework',
+        'complete' : true
+    }, {
+        'name' : 'Feed cats',
+    	'complete' : true
+    }, {
+        'name' : 'call mom',
+		'complete' : false
+    }, {
+        'name' : 'Feed the dog',
+        'complete' : false
+    }];
 
-    var ToDo = ['Do homework', 'Feed cats', 'Call mom', 'Go to the grocery store', 'Clean up'];
+document.write('<h2>My To Do List</h2>');
+document.write('<ul>');
+
+for(var i= 0; i < tasks.length; i++) {
+    try {
+        printTask (tasks[i]);
+    } catch (msg) {
+        console.log ('There is a problem: ' + msg);
+    }
+}
+
+document.write ('</ul>');
+
+function printTask(task) {
+    if(typeof task.name == 'undefined') {
+        throw 'The name of this task is missing! Help!';
+    } else if (typeof task.complete == 'undefined') {
+        throw 'The completion status of this task is gone. OH NO'
+    }
     
-    document.write('<ul>');
-
-        for(var i = 0; i < ToDo.length; i++) {
-        document.write('<li>' + ToDo[i] + '</li>');
-        }
-
-    document.write('</ul>');
-
+    if (task['complete']) {
+        document.write ('<li class="complete">' + task.name + '</li>');
+    } else {
+        document.write ('<li>' + task.name + '</li>'); 
+    }
+}
